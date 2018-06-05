e41c55b06f923a1b004cd95361a961886ea6d8c4
YXB123 <2366705745@QQ.com>
2018-06-05 22:04:00 +0800

    这个项目  是手动把网页源码保存在text.py中的,然后下载图片

---
+++ src/deom/__init__.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python2
+# -*- coding: UTF-8 -*-
+'''
+url  = 'http://tieba.baidu.com/p/2256306796'
+page = urllib.urlopen(url)
+print page.read()
+'''
+
+
+'''
+这个项目  是手动把网页源码保存在text.py中的,然后下载图片
+
+'''
+
+
+import os
+import re
+import urllib
+
+
+
+def read_html_code():
+    #网页的源码保存在text.py中 , 只需要用正则表达式筛选一下就可以把图片的链接提取出来了
+    file_object = open('test.py') 
+    try:
+        #file_context是网页的源码
+        file_context = file_object.read() 
+        print 'file_context----网页的源码是 : ',file_context
+        print '-----------------------------------------------------------------------------------------------'
+        
+        #正则表达式筛选链接
+                #re.compile(r'<img.+?src="(.+?\.jpg)" width')  
+        jpgReg = re.compile(r'<img.+?src="(.+?\.jpg)" width')
+
+        #筛选的结果是一个集合 list []
+        jpgs = re.findall(jpgReg, file_context)
+        print    '筛选的结果是一个集合 list [] ---- ' ,jpgs
+        return jpgs
+    
+    finally:
+        file_object.close()
+        
+        
+# 用图片url下载图片并保存成制定文件名
+def downloadJPG(imgUrl,fileName):
+    urllib.urlretrieve(imgUrl,fileName)
+    
+        
+# 批量下载图片，默认保存到当前目录下
+def batchDownloadJPGs(jpgs_url_list,path = './'):
+    # 用于给图片命名
+    count = 1
+    for url in jpgs_url_list:
+        downloadJPG(url,''.join([path,'{0}.jpg'.format(count)]))
+        count = count + 2
+
+               
+if __name__ == '__main__':
+     HTML__Code = read_html_code()
+     batchDownloadJPGs(HTML__Code)
+
+
---
+++ src/deom/test.py
@@ -0,0 +1,16 @@
+                    
+                    <a data-field='{&quot;un&quot;:&quot;s419655435&quot;}'alog-group="p_author" class="p_author_name j_user_card" href="/home/main?un=s419655435&ie=utf-8&fr=pb" target="_blank"><img src="//tb1.bdstatic.com/tb/cms/nickemoji/2-16.png" class="nicknameEmoji" style="width:13px;height:13px"/>dhs</a>
+                    
+                </li>
+                
+                <li class="l_badge" style="display:block;">
+                    <div class="p_badge">
+                        <a href="/f/like/level?kw=%E6%A1%8C%E9%9D%A2&ie=utf-8&lv_t=lv_nav_intro" target="_blank" class="user_badge d_badge_bright d_badge_icon2_2" title="本吧头衔9级，经验值1168，点击进入等级头衔说明页"><div class="d_badge_title ">进士</div><div class="d_badge_lv">9</div></a>
+                    </div>
+                </li>
+            </ul></div><div class="d_post_content_main ">        <div class="p_content  ">        <div class="save_face_bg_hidden save_face_bg_0"><a rel="noopener" class="save_face_card"></a></div>                <cc>            <div id="post_content_31228127222" class="d_post_content j_d_post_content ">            <img class="BDE_Image" src="https://imgsa.baidu.com/forum/w%3D580/sign=6400ea9250da81cb4ee683c56267d0a4/782209f790529822bb603ba5d6ca7bcb0a46d402.jpg" width="560" height="314" pic_type="0"><br><img class="BDE_Image" src="https://imgsa.baidu.com/forum/w%3D580/sign=4da50e8db7fd5266a72b3c1c9b199799/a623720e0cf3d7cab7a53d10f31fbe096b63a902.jpg" width="560" height="314" pic_type="0"><br><img class="BDE_Image" src="https://imgsa.baidu.com/forum/w%3D580/sign=b00697ba09fa513d51aa6cd60d6c554c/b25594eef01f3a290c7cd7a89825bc315c607c24.jpg" width="560" height="314" pic_type="0"></div><br>            </cc>        <br>        <div class="user-hide-post-down" style="display: none;"></div>                </div>    <a rel="noopener" class="l_post_anchor" name="31228127222l"></a><div class="core_reply j_lzl_wrapper"><div class="core_reply_tail clearfix"><div class="j_lzl_r p_reply" data-field='{&quot;pid&quot;:31228127222,&quot;total_num&quot;:33}'><a rel="noopener" href="#" class="lzl_link_unfold" style="display:none;">回复(33)</a><span class="lzl_link_fold" style="display:">收起回复</span></div><div class="post-tail-wrap"><span class="j_jb_ele"><a rel="noopener" href="#"
+            
+            
+            
+            
+             
