65d593d18b66b6d8fe56486bd73f42fb700d4ec6
e2366705 <2366705745@qq.com>
2018-07-24 23:15:46 +0800

    Update python模拟键盘鼠标操作.py

--- python模拟键盘鼠标操作.py
+++ python模拟键盘鼠标操作.py
@@ -29,6 +29,26 @@ wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww
 
 
 
+'''
+键盘操作:
+k.type_string(‘Hello, World!’) –模拟键盘输入字符串
+k.press_key(‘H’) –模拟键盘按H键
+k.release_key(‘H’) –模拟键盘松开H键
+k.tap_key(“H”) –模拟点击H键
+k.tap_key(‘H’, n = 2, interval = 5) –模拟点击H键，2
+次，每次间隔5秒
+k.tap_key(k.function_keys[5]) –点击功能键F5
+k.tap_key(k.numpad_keys[5], 3) –点击小键盘5, 3次
+
+联合按键模拟
+例如同时按alt + tab键盘
+k.press_key(k.alt_key) –按住alt键
+k.tap_key(k.tab_key) –点击tab键
+k.release_key(k.alt_key) –松开alt键
+
+'''
+
+
 
 
 from selenium import webdriver
