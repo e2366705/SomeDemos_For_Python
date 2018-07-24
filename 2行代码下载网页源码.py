5169932fc4126463fa29f4a1ae38d05faaf25f99
e2366705 <2366705745@qq.com>
2018-07-24 08:20:37 +0800

    Create 2行代码下载网页源码.py

---
+++ 2行代码下载网页源码.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+# coding=utf-8
+import os
+import urllib.request
+
+url = 'http://www.jd.com'
+dir = os.path.abspath('.')          ## 返回当前文件夹
+
+## 开始下载网页文件
+work_path = os.path.join(r'D:\Py\xianyu', 'baiduuuuuuuuuuuuuu.html')   
+urllib.request.urlretrieve(url, work_path)
